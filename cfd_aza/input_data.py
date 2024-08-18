efe03418b042540f082161aebc48218a2d856e8c
AzaFarr <azaliyafarr@mail.ru>
2024-08-19 01:31:54 +0300

    merged

--- cfd_aza/input_data.py
+++ cfd_aza/input_data.py
@@ -15,7 +15,7 @@ class MainData:
 
     def extra_data(self):
         _dx: float = self.length / (self.N - 1)  # length of control volume
-        _delta: float = 0.1  # extra lenght just for correct programm working
+        _delta: float = 0.1  # extra length just for correct program working
         _L = np.arange(start=0, stop=self.length + _delta, step=_dx)  # array with control volumes
 
 
