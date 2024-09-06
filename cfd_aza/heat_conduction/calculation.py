a05d4ccc32040ba05b0cd9308f98099fa02e56b8
AzaFarr <azaliyafarr@mail.ru>
2024-09-06 12:34:02 +0300

    fix 2: start to develop non-stationary one-dimension task of heat conductivity done with OOP

--- cfd_aza/heat_conduction/calculation.py
+++ cfd_aza/heat_conduction/calculation.py
@@ -7,15 +7,16 @@ from cfd_aza.heat_conduction.analytical_solver import analytical_formula
 class Solutions:
     def __init__(self, main_data):
         '''
-          Solving the task of one-dimension stationary heat conductivity
+          Solving the task of one-dimension non-stationary heat conductivity
                  with discrete analogue - TDMA.
           --------------------------------------------------------------
           Parameters:
                N: int  - quantity of control volumes
                length: float  - length of whole thing
-               k: float  - coefficient of heat conductivity
+               k: float  - coefficient of heat conductivity [const]
                T_left: float  - left boundary condition of temperature
                T_right: float  - right boundary condition of temperature
+
                T_numerical: np.ndarray  - array with numerical solutions for each control volume
                T_analytical: np.ndarray  - array with analytical solutions for each control volume
                a, b, c, d: np.ndarray  - arrays with coefficients of discrete analogue
@@ -23,6 +24,7 @@ class Solutions:
           --------------------------------------------------------------
           Extra parameters:
                dx: float  - length of control volume
+               dt: float  - time step
                delta: float  - extra length just for correct program working
                L: np.ndarray  - array with control volumes
           --------------------------------------------------------------
@@ -34,10 +36,17 @@ class Solutions:
         self._k: float = main_data.k
         self._T_left: float = main_data.T_left
         self._T_right: float = main_data.T_right
+        self._c: float = main_data.c
+        self._rho: float = main_data.rho
+        self._S_p: float = main_data.S_p
+        self._S_c: float = main_data.S_c
 
         self._dx: float = self._length / (self._N - 1)
         self._delta: float = 0.1
+        self._dt: float = 0.1
         self._L: np.ndarray = np.arange(start=0, stop=self._length + self._delta, step=self._dx)
+
+        self._T_old_solution: np.ndarray = np.zeros(shape = self._N, dtype = float)
 
         self._T_numerical: np.ndarray = np.zeros(shape = self._N, dtype = float)
         self._T_analytical: np.ndarray = np.zeros(shape = self._N, dtype = float)
@@ -55,14 +64,14 @@ class Solutions:
         self._a[0], self._b[0], self._c[0], self._d[0] = 1, 0, 0, self._T_left
         self._a[self._N - 1], self._b[self._N - 1], self._c[self._N - 1], self._d[self._N - 1] = 1, 0, 0, self._T_right
 
+        self._A = (self._rho * self._c * self._dx) / self._dt
+
         # filling arrays of coefficients with rule of discrete analogue
         for i in range(1, self._N - 1):
-            self._c[i] = self._k_arr[i - 1] / self._dx
-            self._b[i] = self._k_arr[i + 1] / self._dx
-            self._a[i] = self._c[i] + self._b[i]  # a_P = a = b + c + a_Po - (S_p * Dx)
-                                                  # a_Po = rho * c * Dx / Dt
-
-            #self._d[i] = S_c * Dx + a_Po * T_oP  -  что-то там про линеаризованный источниковый член!!!!
+            self._b[i] = self._k_arr[i - 1] / self._dx
+            self._c[i] = self._k_arr[i + 1] / self._dx
+            self._a[i] = self._b[i] + self._c[i] + self._A - (self._S_p * self._dx)
+            self._d[i] = self._S_p * self._dx + self._A * self._T_old_solution[i]
 
 
     def thomas_solution(self):
--- cfd_aza/heat_conduction/io_data.py
+++ cfd_aza/heat_conduction/io_data.py
@@ -10,6 +10,13 @@ class InputData:
     T_left: float
     T_right: float
 
+    # These are for thermal source linearization S = S_c + S_p * T[i]
+    S_p: float
+    S_c: float
+
+    c: float    # specific heat [const]
+    rho: float    # density
+
 class OutputData:
     T_numerical = np.ndarray
     T_analytical = np.ndarray
