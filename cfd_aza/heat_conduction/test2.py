19ff84a28234d118f4ed750dfa078d06ec562a2e
AzaFarr <azaliyafarr@mail.ru>
2024-11-05 09:50:57 +0300

    fix 15

--- cfd_aza/heat_conduction/test2.py
+++ cfd_aza/heat_conduction/test2.py
@@ -1,7 +1,7 @@
-import numpy as np
-import matplotlib.pyplot as mp
-
 import time
+
+import matplotlib.pyplot as mp
+import numpy as np
 
 """ 
 ---------------------
@@ -9,38 +9,58 @@ import time
 ---------------------
 """
 
+
+########################################################################################################################
+
+# функции
+
 def tdma_algorithm(
+        # это функция для реализации
+        # алгоритма TDMA
         a_p: np.ndarray,
         a_w: np.ndarray,
         a_e: np.ndarray,
         b: np.ndarray,
-        N: int,
-        T: np.ndarray) -> np.ndarray:
-    '''
-      Numerical solution for one-dimension unsteady heat conductivity
-         with TDMA (Thomas-algorithm)
-    '''
+        temp_size: int,
+        temp_arr: np.ndarray) -> np.ndarray:
+    """
+    Это функция для реализации
+    алгоритма TDMA
 
-    P = np.zeros(N)
-    Q = np.zeros(N)
+    Parameters
+    ----------
+    a_p
+    a_w
+    a_e
+    b
+    temp_size
+    temp_arr
+
+    Returns
+    -------
+
+    """
+
+    P = np.zeros(temp_size)
+    Q = np.zeros(temp_size)
     P[0] = a_w[0] / a_p[0]
     Q[0] = b[0] / a_p[0]
-    for i in range(1, N):
+    for i in range(1, temp_size):
         P[i] = a_w[i] / (a_p[i] - (a_e[i] * P[i - 1]))
         Q[i] = (b[i] + (a_e[i] * Q[i - 1])) / (a_p[i] - (a_e[i] * P[i - 1]))
 
-    T[N - 1] = Q[N - 1]
-    for i in range(N - 1, 0, -1):
-        T[i - 1] = P[i - 1] * T[i] + Q[i - 1]
+    temp_arr[temp_size - 1] = Q[temp_size - 1]
+    for i in range(temp_size - 1, 0, -1):
+        temp_arr[i - 1] = P[i - 1] * temp_arr[i] + Q[i - 1]
 
-    # T[0] = T_left
-    # T[N-1] = T_right
+    # temp_arr[0] = T_left
+    # temp_arr[temp_size-1] = T_right
 
-    return T
+    return temp_arr
 
-def discrete_analogue(   # это функция для коэф дискр аналога,
-                         # она не используется, т.к. раскрыта
-                         # в цикле в теле основной программы
+
+# -----------------------------------------------------------------------------------------------------------------------
+def discrete_analogue(
         k_arr: np.ndarray,
         a_p: np.ndarray,
         a_w: np.ndarray,
@@ -50,83 +70,118 @@ def discrete_analogue(   # это функция для коэф дискр ан
         S_p: float,
         a_o: float,
         S_c: float,
-        T_old_solution_numerical: np.ndarray,
+        temp_arr: np.ndarray,
+        temp_size: int,
         T_left: float,
         T_right: float) -> None:
+    """
+    Это функция для коэф дискр аналога,
+    она не используется, т.к. раскрыта
+    в цикле в теле основной программы
 
-    # boundary conditions for coefficients
-    # a[0], b[0], c[0], d[0] = 1, 0, 0, T_left  # self.T_old_solution_numerical[0]
-    # a[N - 1], b[N - 1], c[N - 1], d[N - 1] = 1, 0, 0, T_right  # self.T_old_solution_numerical[N - 1]
-    a_p[0], a_w[0], a_e[0], b[0] = 1, 0, -1, 2 * T_left   # коэф для фиктивного к.о.
-    a_p[N - 1], a_w[N - 1], a_e[N - 1], b[N - 1] = 1, -1, 0, 2 * T_right
-    # a[0], b[0], c[0], d[0] = 1, 0, 0, 2*T_left/3  # self.T_old_solution_numerical[0]
-    # a[N - 1], b[N - 1], c[N - 1], d[N - 1] = 1, 0, 0, 2*T_right/3  # self.T_old_solution_numerical[N - 1]
+    Parameters
+    ----------
+    k_arr
+    a_p
+    a_w
+    a_e
+    b
+    dx
+    S_p
+    a_o
+    S_c
+    temp_arr
+    temp_size
+    T_left
+    T_right
 
+    Returns
+    -------
 
-    for i in range(1, N - 1):
+    """
+    a_p[0], a_w[0], a_e[0], b[0] = 1, 0, -1, 2 * T_left  # коэф для фиктивного к.о.
+    a_p[temp_size - 1], a_w[temp_size - 1], a_e[temp_size - 1], b[temp_size - 1] = 1, -1, 0, 2 * T_right
+    # a_p[0], a_w[0], a_e[0], b[0] = 1, 0, 0, T_left  # self.T_old_solution_numerical[0]  # коэф для фиктивного к.о.
+    # a_p[temp_size - 1], a_w[temp_size - 1], a_e[temp_size - 1], b[temp_size - 1] = 1, 0, 0, T_right  # self.T_old_solution_numerical[N - 1]
+    # a_p[0], a_w[0], a_e[0], b[0] = 1, 0, 0, 2*T_left/3  # self.T_old_solution_numerical[0]  # коэф для фиктивного к.о.
+    # a_p[temp_size - 1], a_w[temp_size - 1], a_e[temp_size - 1], b[temp_size - 1] = 1, 0, 0, 2*T_right/3  # self.T_old_solution_numerical[N - 1]
+
+    for i in range(1, temp_size - 1):
         a_w[i] = k_arr[i - 1] / dx
         a_e[i] = k_arr[i + 1] / dx
         a_p[i] = a_w[i] + a_e[i] + a_o - (S_p * dx)
-        b[i] = S_c * dx + a_o * T_old_solution_numerical[i]
+        b[i] = S_c * dx + a_o * temp_arr[i]
 
+
+########################################################################################################################
+# тело программы
+########################################################################################################################
 
 
 # данные, касающиеся самого тела
-N: int = 7
-length: float = 10.0
-k: float = 1000.0
-T_left: float = 100.0
-T_right: float = 500.0
-q_left: float = 1000 # поток слева
-q_right: float = 0 # поток справа
-delta: float = 0.1
-dx: float = length / (N - 1)
+N: int = 7  # количество к.о.
+length: float = 10.0  # длина всего предмета, m
+k: float = 1000.0  # коэффициент температуровпроводности, m^2 / sec
+T_left: float = 100.0  # температура слева, K
+T_right: float = 500.0  # температура справа, K
+q_left: float = 1000  # поток слева, W / m^2
+q_right: float = 0  # поток справа, W / m^2
+delta: float = 0.1  # m
+dx: float = length / (N - 1)  # m
 L: np.ndarray = np.arange(start=0, stop=length + delta, step=dx)
 # c: float = main_data.c
 
 # данные, касающиеся времени
-all_time: float = 100.0
-time_steps: int = 5
-dt: float = all_time / (time_steps - 1)
+all_time: float = 100.0  # все рассматриваемое время, sec
+time_steps: int = 5  # количесвто врем промежутков
+dt: float = all_time / (time_steps - 1)  # sec
 a_o: float = (k * dx) / dt  # a_o = (rho * c * dx) / Dt
 
 # линеаризация источника S = S_c + S_p * T[i]
-S_c = 0
-S_p = 0
+S_c: float = 0.0
+S_p: float = 0.0
 
-T_init: float = T_left # начальная температура
-T_old_solution_numerical: np.array = np.array([], dtype = float)
-T_current_solution_numerical: np.ndarray = T_init * np.ones(shape = N, dtype = float)
+T_init: float = T_left  # начальная температура, K
+T_old_solution_numerical: np.array = np.array([], dtype=float)
+T_current_solution_numerical: np.ndarray = T_init * np.ones(shape=N, dtype=float)
 
-k_arr: np.ndarray = np.array([k] * (N + 1), float) # коэф теплопроводности
+k_arr: np.ndarray = np.array([k] * (N + 1), float)  # массив для коэф теплопроводности
 
-a_p: np.ndarray = np.zeros(shape = N, dtype = float)
-a_w: np.ndarray = np.zeros(shape = N, dtype = float)
-a_e: np.ndarray = np.zeros(shape = N, dtype = float)
-b: np.ndarray = np.zeros(shape = N, dtype = float)
+# массивы для коэф дискретного аналога
+a_p: np.ndarray = np.zeros(shape=N, dtype=float)
+a_w: np.ndarray = np.zeros(shape=N, dtype=float)
+a_e: np.ndarray = np.zeros(shape=N, dtype=float)
+b: np.ndarray = np.zeros(shape=N, dtype=float)
 
+########################################################################################################################
 
 # цикл с решением уравнений
-time_iter: float = 0.0
+
+time_iter: float = 0.0  # текущее время
 while (time_iter <= all_time):
-    a_p[0], a_w[0], a_e[0], b[0] = 1, 0, -1, (2 * T_left) # учитываем, что поток и темепратура заданы на фиктивном к.о.
-    a_p[N - 1], a_w[N - 1], a_e[N - 1], b[N - 1] = 1, -1, 0, (2 * T_right) # учитываем, что поток и темепратура заданы на фиктивном к.о.
+    a_p[0], a_w[0], a_e[0], b[0] = 1, 0, -1, (2 * T_left)  # учитываем, что поток и температура заданы на фиктивном к.о.
+    a_p[N - 1], a_w[N - 1], a_e[N - 1], b[N - 1] = 1, -1, 0, (
+            2 * T_right)  # учитываем, что поток и температура заданы на фиктивном к.о.
     for i in range(1, N - 1):
         a_w[i] = k / dx
         a_e[i] = k / dx
         a_p[i] = a_w[i] + a_e[i] + a_o - (S_p * dx)
         b[i] = S_c * dx + a_o * T_current_solution_numerical[i] + q_left + q_right
-    T_current_solution_numerical = tdma_algorithm(a_p, a_w, a_e, b, N, T_current_solution_numerical) # получаем решение на данном временном шаге
-    T_old_solution_numerical = np.concatenate((T_old_solution_numerical, T_current_solution_numerical)) # записываем отдельно все эти решения
+    T_current_solution_numerical = tdma_algorithm(a_p, a_w, a_e, b, N,
+                                                  T_current_solution_numerical)  # получаем решение на данном временном шаге
+    T_old_solution_numerical = np.concatenate(
+        (T_old_solution_numerical, T_current_solution_numerical))  # записываем отдельно все эти решения
     print(time_iter, ' sec:  ', T_current_solution_numerical)
     time_iter += dt
 
 T_old_solution_numerical = T_old_solution_numerical.reshape((time_steps, N))
 
+# -----------------------------------------------------------------------------------------------------------------------
 
 # отрисовка
-time_iter: float = 0.0
-i: int =  0
+
+time_iter: float = 0.0  # текущее время
+i: int = 0  # номер итерации
 while (time_iter <= all_time):
     fig, ax = mp.subplots()
     line, = ax.plot(L, T_old_solution_numerical[i], "-*m", label='[T] numerical')
@@ -141,25 +196,4 @@ while (time_iter <= all_time):
     time_iter += dt
     i += 1
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+########################################################################################################################
