307872ff32c51dac8e761d3c73ec1cc355a4d4fa
AzaFarr <azaliyafarr@mail.ru>
2025-02-24 14:52:44 +0300

    fix 8: "all works wrong"

--- cfd_aza/solvers/diffusion/test1.py
+++ cfd_aza/solvers/diffusion/test1.py
@@ -71,7 +71,7 @@ top = 'dirichlet'
 bottom = 'neumann'
 
 # данные, касающиеся самой системы
-N_origin: int = 10  # количество к.о.
+N_origin: int = 5  # количество к.о.
 N: int = N_origin + 2  # количество к.о. с учетом фиктивных к.о.
 length: float = 0.010  # длина всего объекта, m
 delta: float = 0.000001  # m
@@ -80,16 +80,18 @@ L: np.ndarray = np.arange(start=(-dz / 2), stop=length + (dz / 2) + delta, step=
 L[0], L[N - 1] = 0, L[N - 1] - (dz / 2)
 
 r: float = 2e-6  # радиус частицы
-rho_partical: float = 1070  # плотность частицы
+rho_partical: float = 1090  # плотность частицы
 rho_fluid: float = 1000  # плотность жидкости
 myu: float = 1e-4  # коэффициент вязкости
 D: float = 1e-10 # коэффициент диффузии
 g: float = 9.81  # ускорение свободного падения
-v: float = -(2 / 9) * r ** 2 * g * ((rho_partical - rho_fluid) / myu)
+# v: float = -(2 / 9) * r ** 2 * g * ((rho_partical - rho_fluid) / myu)
+v: float = 0
 print("v = ", v)
+V: np.ndarray = v * np.ones(shape=N, dtype=float)
 
-C_top: float = 0
-C_bottom: float = 0
+C_top: float = 0.1
+C_bottom: float = 0.1
 q = 0
 
 C_init: float = 0.1  # начальная концентрация
@@ -98,21 +100,21 @@ C_old_solution = C_init * np.ones(shape=N, dtype=float)  # массив для �
 C_old_solution_set: np.array = np.array([], dtype=float)  # массив для записи всех решений
 
 # данные, касающиеся времени
-all_time: float = 30000.0  # все рассматриваемое время, sec
+all_time: float = 4.0  # все рассматриваемое время, sec
 dt: float = 1  # шаг по времени для расчета, sec
 time_steps: int = int(all_time / dt)  # количество врем промеж-в для расчета
 a_o: float = dz / dt
 
 # ДАННЫЕ ДЛЯ ОТРИСОВКИ ПО ЗАДАННОМУ ВРЕМЕННОМУ ШАГУ
-# AAAA: float = 6000  # шаг по времени для отрисовки
-# FFF = time_steps // AAAA + 1  # количество врем промеж-в для отрисовки
+AAAA: float = 1  # шаг по времени для отрисовки
+FFF = time_steps // AAAA + 1  # количество врем промеж-в для отрисовки
 
-# ДАННЫЕ ДЛЯ ОТРИСОВКИ В ОПРЕДЕЛЕННОМ ВРЕМ-ОМ ПРОМЕЖ-КЕ ПО ЗАДАННОМУ КОЛИЧЕСТВУ ВРЕМ-Х ПРОМЕЖ-В
-BBB: float = 5
-Start: float = 2000
-End: float = 20000
-AAAA: float = (End - Start) / BBB  # шаг по времени для отрисовки
-FFF = BBB  # количество врем промеж-в для отрисовки
+# # ДАННЫЕ ДЛЯ ОТРИСОВКИ В ОПРЕДЕЛЕННОМ ВРЕМ-ОМ ПРОМЕЖ-КЕ ПО ЗАДАННОМУ КОЛИЧЕСТВУ ВРЕМ-Х ПРОМЕЖ-В
+# BBB: float = 5
+# Start: float = 2000
+# End: float = 80000
+# AAAA: float = (End - Start) / BBB  # шаг по времени для отрисовки
+# FFF = BBB  # количество врем промеж-в для отрисовки
 
 
 
@@ -138,14 +140,16 @@ while (time_iter <= all_time):
 
     rho = rho_partical * C_old_solution + rho_fluid * (1 - C_old_solution)
     f = rho_partical / rho
+    V = (1 - C_old_solution) ** 2 * v
 
-    a_p[0], a_w[0], a_e[0], b[0] = ((D / dz) + ((1 - f[0]) * v)), 0, (D / dz), C_top * (1 - f[0]) * v  # учитываем фиктивный к.о.
-    # a_p[0], a_w[0], a_e[0], b[0] = 1, 0, 1, -q  # учитываем фиктивный к.о.
-    a_p[N - 1], a_w[N - 1], a_e[N - 1], b[N - 1] = (D / dz), ((D / dz) + ((1 - f[N - 1]) * v)), 0, -C_bottom * (1 - f[N - 1]) * v
+    # a_p[0], a_w[0], a_e[0], b[0] = ((D / dz) + ((1 - f[0]) * V[0] / 2)), 0, ((D / dz) - ((1 - f[0]) * V[0] / 2)), C_top * V[0]  # учитываем фиктивный к.о.
+    # a_p[N - 1], a_w[N - 1], a_e[N - 1], b[N - 1] = (D / dz) - ((1 - f[N - 1]) * V[N - 2] / 2), ((D / dz) + ((1 - f[N - 1]) * V[N - 2] / 2)), 0, -C_bottom * (1 - f[N - 1]) * V[N - 2]
+    a_p[0], a_w[0], a_e[0], b[0] = 1, 0, -1, 2 * C_top # учитываем фиктивный к.о.
+    a_p[N - 1], a_w[N - 1], a_e[N - 1], b[N - 1] = 1, -1, 0, 2 * C_bottom
 
 
     for i in range(1, N - 1):
-        a_w[i] = (D / dz) + (v * (1 - f[i]))
+        a_w[i] = (D / dz) + (V[i] * (1 - f[i]))
         a_e[i] = D / dz
         a_p[i] = a_w[i] + a_e[i] + a_o
         b[i] = a_o * C_old_solution[i]
@@ -153,12 +157,12 @@ while (time_iter <= all_time):
     C_current_solution_numerical = tdma_algorithm(a_p, a_w, a_e, b, N,
                                                   C_old_solution)  # получаем решение на данном временном шаге
     C_old_solution = C_current_solution_numerical
-    C_current_solution_numerical[0] = (C_current_solution_numerical[0] + C_current_solution_numerical[1]) / 2
-    C_current_solution_numerical[N - 1] = (C_current_solution_numerical[N - 2] + C_current_solution_numerical[
-        N - 1]) / 2
+    # C_current_solution_numerical[0] = (C_current_solution_numerical[0] + C_current_solution_numerical[1]) / 2
+    # C_current_solution_numerical[N - 1] = (C_current_solution_numerical[N - 2] + C_current_solution_numerical[
+    #     N - 1]) / 2
 
-    # if (time_iter % AAAA == 0):
-    if (time_iter % AAAA == 0) and (time_iter >= Start) and (time_iter <= End):
+    if (time_iter % AAAA == 0):
+    # if (time_iter % AAAA == 0) and (time_iter >= Start) and (time_iter <= End):
         C_old_solution_set = np.concatenate(
             (C_old_solution_set, C_current_solution_numerical))  # записываем отдельно все эти решения
         concentration[iter] = np.sum(C_current_solution_numerical) / N
@@ -191,19 +195,19 @@ print("integral = ", integral)
 
 # отрисовка
 
-# time_iter: float = 0  # текущее время
-# i: int = 0  # номер итерации
-# while (time_iter <= all_time):
-#     mp.plot(L, C_old_solution_set[i], "-*", label='%d сек' % (time_iter))
-#     time_iter += AAAA
-#     i += 1
-
-time_iter: float = Start  # текущее время
+time_iter: float = 0  # текущее время
 i: int = 0  # номер итерации
-while (time_iter < End):
+while (time_iter <= all_time):
     mp.plot(L, C_old_solution_set[i], "-*", label='%d сек' % (time_iter))
     time_iter += AAAA
     i += 1
+
+# time_iter: float = Start  # текущее время
+# i: int = 0  # номер итерации
+# while (time_iter < End):
+#     mp.plot(L, C_old_solution_set[i], "-*", label='%d сек' % (time_iter))
+#     time_iter += AAAA
+#     i += 1
 
 mp.legend()
 mp.xlabel('Length, [m]')
--- cfd_aza/solvers/diffusion/test2.py
+++ cfd_aza/solvers/diffusion/test2.py
@@ -71,16 +71,16 @@ top = 'dirichlet'
 bottom = 'neumann'
 
 # данные, касающиеся самой системы
-N_origin: int = 40  # количество к.о.
+N_origin: int = 70  # количество к.о.
 N: int = N_origin + 2  # количество к.о. с учетом фиктивных к.о.
-length: float = 0.010  # длина всего объекта, m
+length: float = 0.01  # длина всего объекта, m
 delta: float = 0.000001  # m
 dz: float = length / N_origin  # m
 L: np.ndarray = np.arange(start=(-dz / 2), stop=length + (dz / 2) + delta, step=dz)
 L[0], L[N - 1] = 0, L[N - 1] - (dz / 2)
 
 r: float = 2e-6  # радиус частицы
-rho_partical: float = 1300  # плотность частицы
+rho_partical: float = 1080  # плотность частицы
 rho_fluid: float = 1000  # плотность жидкости
 myu: float = 1e-4  # коэффициент вязкости
 D: float = 1e-10 # коэффициент диффузии
@@ -98,9 +98,9 @@ C_old_solution = (C_init / N) * np.ones(shape=N, dtype=float)  # массив д
 C_old_solution_set: np.array = np.array([], dtype=float)  # массив для записи всех решений
 
 # данные, касающиеся времени
-all_time: float = 50000.0  # все рассматриваемое время, sec
+all_time: float = 100000.0  # все рассматриваемое время, sec
 dt: float = 1  # sec
-AAAA: float = 10000
+AAAA: float = 20000
 time_steps: int = int(all_time / dt)  # количество врем промежутков
 a_o: float = dz / dt
 
@@ -125,7 +125,7 @@ while (time_iter <= all_time):
 
     rho = rho_partical * C_old_solution + rho_fluid * (1 - C_old_solution)
     f = rho_partical / rho
-    V = abs(C_init - C_old_solution) * v
+    V = (1 - (C_old_solution/C_init)) * v
 
     # a_p[0], a_w[0], a_e[0], b[0] = ((D / dz) + ((1 - f[0]) * V[0] / 2)), 0, (D / dz), C_top * (1 - f[0]) * V[0]  # учитываем фиктивный к.о.
     # # a_p[0], a_w[0], a_e[0], b[0] = 1, 0, 1, C_top  # учитываем фиктивный к.о.
