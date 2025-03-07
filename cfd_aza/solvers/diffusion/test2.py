6be1393ad6b8c90eb07e66a2c945f1dfc7739c6a
AzaFarr <azaliyafarr@mail.ru>
2025-03-07 12:18:20 +0300

    fix 14: "немного упорядочила"

--- cfd_aza/solvers/diffusion/test2.py
+++ cfd_aza/solvers/diffusion/test2.py
@@ -67,8 +67,8 @@ def tdma_algorithm(
 ########################################################################################################################
 
 #ВЫБРАТЬ ГРАНИЧНЫЕ УСЛОВИЯ:  dirichlet | neumann | robin
-top = 'dirichlet'
-bottom = 'neumann'
+# top = 'dirichlet'
+# bottom = 'neumann'
 
 # данные, касающиеся самой системы
 N_origin: int = 10  # количество к.о.
@@ -85,12 +85,12 @@ rho_fluid: float = 1000  # плотность жидкости
 myu: float = 1e-4  # коэффициент вязкости
 D: float = 1e-10 # коэффициент диффузии
 g: float = 9.81  # ускорение свободного падения
-v: float = -(2 / 9) * r ** 2 * g * ((rho_partical - rho_fluid) / myu)
+v: float = -(2 / 9) * r ** 2 * g * ((rho_partical - rho_fluid) / myu)  # скорость по Стоксу
 V: np.ndarray = v * np.ones(shape=N, dtype=float)
 print("v = ", v)
 
-C_top: float = 0.0
-C_bottom: float = 0.0
+C_top: float = 0.0  # концентрация сверху
+C_bottom: float = 0.0  # концентрация снизу
 
 C_init: float = 0.5  # начальная концентрация
 C_old_solution = (C_init) * np.ones(shape=N, dtype=float)  # массив для записи решения на старом временном слое
@@ -98,10 +98,10 @@ C_old_solution = (C_init) * np.ones(shape=N, dtype=float)  # массив для
 C_old_solution_set: np.array = np.array([], dtype=float)  # массив для записи всех решений
 
 # данные, касающиеся времени
-all_time: float = 3000.0  # все рассматриваемое время, sec
-dt: float = 1  # sec
-AAAA: float = 1000
-time_steps: int = int(all_time / dt)  # количество врем промежутков
+all_time: float = 30000.0  # все рассматриваемое время, sec
+dt: float = 1  # по критерию Курента, sec
+AAAA: float = 10000  # временной шаг для отображения на графике инетресующий момент времени
+time_steps: int = int(all_time / dt)  # количество врем промежутков для расчета
 a_o: float = dz / dt
 
 
@@ -126,17 +126,49 @@ while (time_iter <= all_time):
     f = rho_partical / rho
     V = v * (1 - C_old_solution) ** 2
 
-    # a_p[0], a_w[0], a_e[0], b[0] = 1, 0, -1, 2 * C_top # учитываем фиктивный к.о.
-    # a_p[N - 1], a_w[N - 1], a_e[N - 1], b[N - 1] = 1, -1, 0, 2 * C_bottom
-    # a_p[0], a_w[0], a_e[0], b[0] = ((D / dz) + ((1 - f[0]) * V[0] / 2)), 0, (D / dz), C_top * (1 - f[0]) * V[0]  # учитываем фиктивный к.о.
-    # a_p[0], a_w[0], a_e[0], b[0] = 1, 0, 1, C_top  # учитываем фиктивный к.о.
-    # a_p[N - 1], a_w[N - 1], a_e[N - 1], b[N - 1] = (D / dz), ((D / dz) + ((1 - f[N - 1]) * V[N - 1])), 0, -C_bottom * (1 - f[N - 1]) * V[N - 1]
-    # a_p[N - 1], a_w[N - 1], a_e[N - 1], b[N - 1] = 1, 1, 0, C_bottom  # учитываем фиктивный к.о.
-    a_p[0], a_w[0], a_e[0], b[0] = 1, 0, abs(D / (D + dz * (1 - f[0]) * V[0])), 0 # учитываем фиктивный к.о.
-    a_p[N - 1], a_w[N - 1], a_e[N - 1], b[N - 1] = 1, abs((D + dz * (1 - f[N - 1]) * V[N - 2]) / D), 0, 0
+    # ----- ГУ 1 рода
+    # a_p[0] = 1
+    # a_w[0] = 0
+    # a_e[0] = -1
+    # b[0] = 2 * C_top
+    # a_p[N - 1] = 1
+    # a_w[N - 1] = -1
+    # a_e[N - 1] = 0
+    # b[N - 1] = 2 * C_bottom
+
+    #----- ГУ 2 рода
+    # a_p[0] = 1
+    # a_w[0] = 0
+    # a_e[0] = 1
+    # b[0] = C_top
+    # a_p[N - 1] = 1
+    # a_w[N - 1] = 1
+    # a_e[N - 1] = 0
+    # b[N - 1] = C_bottom
+
+    # ----- ГУ 3 рода (усредненное значение на границе)
+    # a_p[0] = ((D / dz) + ((1 - f[0]) * V[0] / 2))
+    # a_w[0] = 0
+    # a_e[0] = (D / dz)
+    # b[0] = C_top * (1 - f[0]) * V[0]
+    # a_p[N - 1] = (D / dz)
+    # a_w[N - 1] = ((D / dz) + ((1 - f[N - 1]) * V[N - 1]))
+    # a_e[N - 1] = 0
+    # b[N - 1] = -C_bottom * (1 - f[N - 1]) * V[N - 1]
+
+
+    # ----- ГУ 3 рода (схема против потока)
+    a_p[0] = 1
+    a_w[0] = 0
+    a_e[0] = D / (D + dz * (1 - f[0]) * V[0])
+    b[0] = 0
+    a_p[N - 1] = 1
+    a_w[N - 1] = (D + dz * (1 - f[N - 2]) * V[N - 2]) / D
+    a_e[N - 1] = 0
+    b[N - 1] = 0
 
     for i in range(1, N - 1):
-        a_w[i] = abs(D / dz + (V[i] * (1 - f[i])))
+        a_w[i] = D / dz + (V[i] * (1 - f[i]))
         a_e[i] = D / dz
         a_p[i] = a_w[i] + a_e[i] + a_o
         b[i] = a_o * C_old_solution[i]
@@ -144,17 +176,17 @@ while (time_iter <= all_time):
     C_current_solution_numerical = tdma_algorithm(a_p, a_w, a_e, b, N,
                                                   C_old_solution)  # получаем решение на данном временном шаге
     C_old_solution = C_current_solution_numerical
+
     # C_current_solution_numerical[0] = (C_current_solution_numerical[0] + C_current_solution_numerical[1]) / 2
-    # C_current_solution_numerical[N - 1] = (C_current_solution_numerical[N - 2] + C_current_solution_numerical[
-    #     N - 1]) / 2
+    # C_current_solution_numerical[N - 1] = (C_current_solution_numerical[N - 2] + C_current_solution_numerical[N - 1]) / 2
 
 
     if (time_iter % AAAA == 0):
         C_old_solution_set = np.concatenate(
             (C_old_solution_set, C_current_solution_numerical))  # записываем отдельно все эти решения
-        integral[iter] = np.sum(C_old_solution[1 : N - 1] * dz)
+        integral[iter] = np.sum(C_old_solution[1 : N - 1] * dz)  # численный интеграл решений, для проверки выполнения закона сохранения
         iter += 1
-        # print("                ", C_current_solution_numerical)
+        # print("C = ", C_current_solution_numerical)
         print("(1 - f) * V = ", (1 - f) * V)
 
 
@@ -172,11 +204,11 @@ while (time_iter <= all_time):
     # print('         a_w = ', a_w)
     # print('         b = ', b)
     # print('\n\n')
+
     time_iter += dt
 
 
 C_old_solution_set = C_old_solution_set.reshape((time_steps // AAAA + 1, N))
-# C_old_solution_set = C_old_solution_set.reshape((time_steps, N))
 
 print("integral = ", integral)
 
@@ -188,7 +220,7 @@ print("integral = ", integral)
 time_iter: float = 0  # текущее время
 i: int = 0  # номер итерации
 while (time_iter <= all_time):
-    mp.plot(L[1 : N - 1], C_old_solution_set[i][1 : N - 1], "-*", label='%d сек' % (time_iter))
+    mp.plot(L, C_old_solution_set[i], "-*", label='%d сек' % (time_iter))
     time_iter += AAAA
     i += 1
 
